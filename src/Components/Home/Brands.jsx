c2b6fff78f3c54d2954aa1f89063a11d4139f4ac
k-web-tech <kamrulislamapurba@gmail.com>
2025-06-24 22:43:14 +0600

    Trusted Brands logo added

---
+++ src/Components/Home/Brands.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Marquee from "react-fast-marquee";
+
+
+export const Brands = () => {
+    return (
+        <div className='bg-gradient-to-b from-pink-50 to-white py-10'>
+
+            <Marquee>
+                <img src="/blogo/b1.png" alt="brandLogo" className='mx-8 lg:w-60 w-48' />
+                <img src="/blogo/b2.png" alt="brandLogo" className='mx-8 lg:w-60 w-48' />
+                <img src="/blogo/bb3.png" alt="brandLogo" className='mx-8 lg:w-60 w-48' />
+                <img src="/blogo/b4.png" alt="brandLogo" className='mx-8 lg:w-60 w-48' />
+                <img src="/blogo/b5.png" alt="brandLogo" className='mx-8 lg:w-60 w-48' />
+            </Marquee>
+
+        </div>
+    )
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Banner from '../Components/Home/Banner'
+import { Brands } from '../Components/Home/Brands'
 
 export const HomePage = () => {
     return (
         <div>
 
             <Banner />
+
+            <Brands />
 
         </div>
     )
